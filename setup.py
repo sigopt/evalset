918c7d709f39e92f1f0611d3b196e951500be4b4
Patrick Hayes <pfhayes@gmail.com>
2016-11-23 14:48:21 -0800

    Make evalset into module

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from sigopt.version import VERSION
+
+# keep this in sync with requirements.txt
+install_requires=['numpy==1.10.1', 'scipy==0.16.0']
+
+setup(
+  name='evalset',
+  version='1.0.0',
+  description='Benchmark suite of test functions suitable for evaluating black-box optimization strategies',
+  author='SigOpt',
+  author_email='support@sigopt.com',
+  url='https://sigopt.com/',
+  packages=['evalset'],
+  install_requires=install_requires,
+  classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+  ]
+)
